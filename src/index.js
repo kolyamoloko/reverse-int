6e89bdecf891b12d49361b7cc8ad345097ba90bc
kolyamoloko <kolyamoloko@mail.ru>
2021-08-19 12:26:39 +0300

    add: function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let number = 0;
+    let absn = Math.abs(n);
+    while (absn > 0){
+        number = number + absn % 10 ;
+        absn = Math.floor(absn/10);
+        number = number * 10;
+    }
+    return number / 10;
 }
